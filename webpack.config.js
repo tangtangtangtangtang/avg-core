ecddcf22ddebf904ba53421934334fcb8929712c
Icemic <bingfeng.jw@gmail.com>
2017-10-07 01:11:55 +0800

    update babel config, fix yoga transpile

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const BitBarWebpackProgressPlugin = require('bitbar-webpack-progress-plugin');
 
 const packageInfo = require('./package.json');
 
+const fs = require('fs');
+const babelOptions = JSON.parse(fs.readFileSync('./.babelrc', 'utf8'));
+
 /* eslint-disable */
 
 module.exports = function (env) {
@@ -26,7 +29,7 @@ module.exports = function (env) {
 
   return {
     cache: true,
-    entry: ['babel-polyfill', 'whatwg-fetch', './src/avg.js'],
+    entry: ['whatwg-fetch', './src/avg.js'],
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: `avg${env.minimize ? '.min' : ''}.js`,
@@ -39,7 +42,8 @@ module.exports = function (env) {
     },
     module: {
       rules: [
-        { test: /\.js$/, exclude: /node_modules\/(?!(koa-compose|avg-.*|pixi-richtext|huozi))/, loader: 'babel-loader', query: { compact: true } },
+        { test: /\/yoga-layout\/.*entry-.*\.js$/, loader: 'babel-loader', query: { compact: true, cacheDirectory: true, babelrc: false, ...babelOptions } },
+        { test: /\.js$/, exclude: /node_modules\/(?!(koa-compose|avg-.*|pixi-richtext|huozi))/, loader: 'babel-loader', query: { compact: true, cacheDirectory: true } },
         { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader', query: { compact: true, cacheDirectory: true } },
         { test: /\.(glsl|frag|vert)$/, loader: 'raw-loader', exclude: /node_modules/ },
       ],

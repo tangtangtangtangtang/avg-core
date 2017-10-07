ac1217a7ba479b82100878cf33168fae881336c8
Icemic <bingfeng.jw@gmail.com>
2017-10-07 13:44:47 +0800

    use lerna

--- packages/avg-core/webpack.config.js
+++ packages/avg-core/webpack.config.js
@@ -5,7 +5,28 @@ const BitBarWebpackProgressPlugin = require('bitbar-webpack-progress-plugin');
 const packageInfo = require('./package.json');
 
 const fs = require('fs');
-const babelOptions = JSON.parse(fs.readFileSync('./.babelrc', 'utf8'));
+const babelOptions = JSON.parse(fs.readFileSync('../../.babelrc', 'utf8'));
+
+const license = `/*!
+ * @file        AVG.js library
+ * @author      Icemic Jia <bingfeng.web@gmail.com>
+ * @copyright   2015-2017 Icemic Jia
+ * @link        https://www.avgjs.org
+ * @license     Apache License 2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+`;
 
 /* eslint-disable */
 
@@ -60,6 +81,7 @@ module.exports = function (env) {
         },
       }),
       ...uglifyPlugin,
+      new webpack.BannerPlugin({ banner: license, raw: true }),
       new BitBarWebpackProgressPlugin(),
     ]
   };

5a12f85bfbcbe10ec40ffcb1747a70ef937a8fb0
Icemic <bingfeng.jw@gmail.com>
2017-11-18 22:35:12 +0800

    export components

---
+++ packages/avg-components/src/index.js
@@ -0,0 +1,7 @@
+import Image from './lib/image';
+import Container from './lib/container';
+import Layer from './lib/layer';
+import RichText from './lib/richtext';
+import Text from './lib/text';
+
+export { Image, Container, Layer, RichText, Text };
